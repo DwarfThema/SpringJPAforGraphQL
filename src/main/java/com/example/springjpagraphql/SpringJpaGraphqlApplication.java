2605e2a343109a9dd57d12689ff20e9c58d4326b
Junho Park <dwarfthema@gmail.com>
2022-10-03 01:15:12 +0900

    init:all:SpringBoot + JPA + GraphQL

---
+++ src/main/java/com/example/springjpagraphql/SpringJpaGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjpagraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaGraphqlApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/springjpagraphql/entity/BoardEntity.java
@@ -0,0 +1,22 @@
+package com.example.springjpagraphql.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name="BOARD")
+@Entity
+public class BoardEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String content;
+}
